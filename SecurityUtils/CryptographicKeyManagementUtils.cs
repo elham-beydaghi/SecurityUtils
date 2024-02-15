0fa0f5c8556f0e3766ff840db724b652936e7fd5
elham-beydaghi <elhambeydaghi1999@gmail.com>
2024-02-15 23:33:10 +0330

    add hmac algo

--- SecurityUtils/CryptographicKeyManagementUtils.cs
+++ SecurityUtils/CryptographicKeyManagementUtils.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SecurityUtils
 {
     public static class CryptographicKeyManagementUtils
     {
+        public static string HMACComputing(string message, string key)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+            using (HMACSHA256 hmac = new HMACSHA256(keyBytes))
+            {
+                byte[] hashBytes = hmac.ComputeHash(messageBytes);
+                string hmacString = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                return hmacString;
+            }
+        }
     }
 }
